d13ba330a126da3268f39e072d54600b5a62718f
Kazuichi Takashiro <takashiro@qq.com>
2023-08-16 00:09:10 +0800

    Split jest configurations

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,26 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+const commonOptions = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+	coveragePathIgnorePatterns: [
+		'<rootDir>/node_modules/',
+		'<rootDir>/test/',
+	],
+};
+
+const projects = [
+	'qbittorrent',
+	'spider',
+	'vigilia',
+];
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+	projects: projects.map((project) => ({
+		...commonOptions,
+		displayName: project,
+		testMatch: [
+			`<rootDir>/test/${project}/*.spec.ts`,
+		],
+	})),
 };
