859d7a248fed4b7113af69f06dea1ed11f74dc05
Kazuichi Takashiro <takashiro@qq.com>
2023-08-24 00:08:11 +0800

    Add button styles

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Banner from './landmark/Banner';
+import Main from './landmark/Main';
 
 import './global.scss';
 
 export default function App(): JSX.Element {
 	return (
-		<Banner />
+		<>
+			<Banner />
+			<Main />
+		</>
 	);
 }
---
+++ src/web/landmark/Main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Main(): JSX.Element {
+	return (
+		<main>
+			<button type="submit">Submit</button>
+			<button type="button">Cancel</button>
+		</main>
+	);
+}
