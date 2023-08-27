a83cd51b13b7c2728281d3a095b96f94e8985102
Kazuichi Takashiro <takashiro@qq.com>
2023-08-27 21:15:19 +0800

    Do not use function as props

--- src/web/common/Clickable/index.tsx
+++ src/web/common/Clickable/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 interface ClickableProps extends React.HTMLAttributes<HTMLElement> {
+	component?: React.ElementType;
 	disabled?: boolean;
 	onTrigger(e: React.KeyboardEvent<HTMLElement> | React.MouseEvent<HTMLElement>): void;
 }
 
 export default function Clickable({
+	component: Component = 'div',
 	role = 'button',
 	disabled = false,
 	tabIndex = disabled ? -1 : 0,
@@ -15,27 +17,27 @@ export default function Clickable({
 	children,
 	...otherProps
 }: ClickableProps): JSX.Element {
-	function handleClick(e: React.MouseEvent<HTMLElement>): void {
+	const handleClick = (e: React.MouseEvent<HTMLElement>): void => {
 		onTrigger(e);
 		onClick?.(e);
-	}
+	};
 
-	function handleKeyDown(e: React.KeyboardEvent<HTMLElement>): void {
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLElement>): void => {
 		if (!e.altKey && !e.ctrlKey && !e.shiftKey && !e.metaKey && (e.key === ' ' || e.key === 'Enter')) {
 			onTrigger(e);
 		}
 		onKeyDown?.(e);
-	}
+	};
 
 	return (
-		<div // eslint-disable-line jsx-a11y/no-static-element-interactions
+		<Component
 			role={role}
 			tabIndex={tabIndex}
 			onClick={handleClick}
 			onKeyDown={handleKeyDown}
-			{...otherProps} // eslint-disable-line react/jsx-props-no-spreading
+			{...otherProps}
 		>
 			{children}
-		</div>
+		</Component>
 	);
 }
